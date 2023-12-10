d8e700586ab5541f593ba9c590aa55bec0afe279
gitYoungren <youngrengitonga@gmail.com>
2023-12-11 01:41:25 +0300

    done

--- lib/num.py
+++ lib/num.py
@@ -4,8 +4,9 @@ def two_are_positive(a, b, c):
         return False
     elif (a > 0 and b > 0) or (a > 0 and c > 0) or (c > 0 and b > 0):
         return True
+
     return False
 
-    result = two_are_positive(5, 6, -9)
+    result = two_are_positive(1, 3, -1)
 
-    print (result)
+    print(result)
